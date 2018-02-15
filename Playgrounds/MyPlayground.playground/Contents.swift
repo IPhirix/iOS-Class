3cd400d7508fc90a0ab0fe29bfbde01c9da094b6
Jason Wilson <jason@Jasons-iMac.local>
2018-02-14 19:24:28 -0600

    Added some more chapter notes

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -7,6 +7,7 @@ import UIKit
 
 
 
+//--------------------- Basics ---------------------
 //Variables are not a constant
 //= is an assignment operator
 var message = "Hello, world!"
@@ -23,7 +24,9 @@ var bankAccountBalance = 100
 var cashRegisterMessage = bankAccountBalance >= 50 ? "You just bought the item" : "You don't have enough money to buy the item"
 
 
-//Strings
+
+
+//--------------------- Strings ---------------------
 //The below example is of type inferance. the variable 'str' has a 'string' type data assigned to it
 //You're not explicitely declaring the type. It's infering what type it is
 var str = "Hello, playground"
@@ -32,9 +35,13 @@ var str = "Hello, playground"
 //Explicitely declared type, 'String'
 var str2: String = "Hello, playground"
 
-var firstName = "Jason"
-var lastName = "Wilson"
+//"let" makes it a constant not a variable. It can't be changed or edited
+//something that is able to change is called mutable, based on the word mutate, which just means “to change”.
+//Something that is unable to be changed is called immutable
+let firstName = "Jason"
+let lastName = "Wilson"
 var age = 27
+
 
 //String concatination
 //Also called string manipulation
@@ -44,6 +51,11 @@ var fullName = firstName + " " + lastName
 //String interpolation
 //\(variable goes here)
 var fullName2 = "\(firstName) \(lastName) is \(age)"
+
+//String interpolation
+let herFirstName = "Tara"
+let city = "Paris"
+let welcomeString = "Hello " + herFirstName + ", welcome to " + city + "!"
 
 //appending property
 fullName.append(" III")
@@ -64,7 +76,16 @@ if sentence.contains("fuck") || sentence.contains("hell") {
     sentence.replacingOccurrences(of: "hell", with: "heck")
 }
 
+//Using swift symbols in a string: use \ before the symbol (escape character) followed by a special symbol is called an (escape sequence)
+let stringWithQuotationMarks = "He said, \"Hi there!\" as he passed by."
 
+//New line \n
+let startOfAPoem = "Roses are red.\nViolets are blue."
+
+
+
+
+//--------------------- numbers ---------------------
 //integers, floats, and doubles
 var myAge = 28
 var weight: Int = 240
@@ -92,13 +113,230 @@ else {
     print("This is an odd number")
 }
 
+//-=, += take score add or subtract two and then assign it back to score
+//compound assignment operator
+var score = 0
+score += 2
+
+var greeting = ""
+greeting += "Hello"
+greeting += " "
+greeting += "World"
+
+
+
+//--------------------- print & logging ---------------------
+//Printing messages to the console is known as logging and the messages are sometimes called log messages.
+var userNum1 = 7
+var userNum2 = 4
+
+if userNum1 - userNum2 < 0 {
+    print("The result is negative")
+}
+else {
+    print("The result is positive")
+}
+
+
+
+//--------------------- Functions ---------------------
+//The idea of taking something complex and defining a simpler way to refer to it is called - abstraction
+// {} is considered the body. So the print statements are in the body of the rowTheBoat function
+// They often break the single long list down into multiple smaller lists. This is called decomposition
+func rowTheBoat() {
+    print("Row, row, row your boat")
+    print("Gently down the stream")
+}
+
+
+let userName = "Jason"
+
+func sayHiToUser(){
+    print("Hello, \(userName)! How's it going today?")
+}
+
+func askUser(){
+    print("What would you like to do?")
+}
+
+func goodMorning(){
+        sayHiToUser()
+        askUser()
+}
+
+goodMorning()
+
+
+
+
+//--------------------- Type --------------------
+//trying to assign the wrong type of value to a variable caused an error. In fact, Swift won’t let you write code that uses types incorrectly or unexpectedly. This is called type safety
+
+
+//When you write a value in code — like 42 or "hello" — it’s known as a literal.
+//When Swift uses context clues from code to infer what type something is, it’s called type inference.
+
+//The error Type annotation missing in pattern means that Swift is unable to infer — work out from the available information — the type
+
+
+//Type annotation - (: Double)
+let annotatedDouble: Double = 20
+
+
+//Types use Upper camel case
+//You can create your own “Type”
+//(TrainingShoe: it could have: int for shoe size, string for brand name, and date for its release)
+
+//Types and capabilities can be grouped together into collections called frameworks or libraries.
+//The Foundation framework introduces lots of types used to represent more specific things than just strings or numbers from the Swift standard library. For example, there are types for dates, distances, and files on a computer.
+
+import Foundation;
+
+let today = Date()
+
+
+
+
+//--------------------- Functions, Parameters, and arguments --------------------
+func hello(name: String) {
+    print("Hello " + name)
+}
+
+hello(name: "Maria")
+hello(name: "Vikram")
+
+//when someone uses the function, they can tell the function what the value of the name parameter should be. This is called passing in a value. The value you pass in to the function is called the argument.
+
+
+func helloThere(firstName: String, lastName: String) {
+    print("Hello \(firstName) \(lastName)")
+}
+
+helloThere(firstName: "Jason", lastName: "Wilson")
+
+
+func printFavorite(categoryOfThing: String, favorite: String){
+    print("My favorite \(categoryOfThing) \(favorite)")
+}
+
+printFavorite(categoryOfThing: "Food", favorite: "Pizza")
+
+//Passing a value back when a function is finished is called returning a value
+//Your function can have multiple parameters, but it can only return one value.
+func spaceAvailableMessage(eachVideoDuration: Int, numberOfVideos: Int) -> String {
+    let currentSpace = 10000
+    let megabytesPerVideoSecond = 3
+    let spaceAvailable = currentSpace - eachVideoDuration * numberOfVideos * megabytesPerVideoSecond
+    
+    return "If your \(numberOfVideos) videos are \(eachVideoDuration) seconds each, you'll have \(spaceAvailable) MBs remaining"
+}
+spaceAvailableMessage(eachVideoDuration: 10, numberOfVideos: 50)
+
+
+func yourFavoriteThing(favoriteTopic: String, favoriteItem: String)->String{
+    return "Your favorite thing \(favoriteTopic) is: \(favoriteItem)"
+}
+
+yourFavoriteThing(favoriteTopic: "Food", favoriteItem: "Pizza")
+
+
+//Taking it to the next level
+func makeFavorite(categoryOfThing: String, favorite: String)->String{
+    return "My favorite \(categoryOfThing) is \(favorite)"
+}
+
+let favoriteFood = makeFavorite(categoryOfThing: "Food", favorite: "cheese")
+let favoriteMovie = makeFavorite(categoryOfThing: "Movie", favorite: "Hot Rod")
+let favoriteSport = makeFavorite(categoryOfThing: "Sport", favorite: "Basketball")
+
+print("Hello, my name is Jason. \(favoriteFood) \(favoriteMovie) \(favoriteSport)")
+
+//When you name a function, it’s good to somehow include the side effect in the name, like leftArmUp(). If a function has no return value, all of its work is considered a side effect.
+
+
+func printHello(to: String) {
+    print ("Hello " + to)
+}
+printHello(to: "Maya")
+
+
+//The names used inside the function (the values that have been received) are parameter names.
+//In fact, there are better words for these two names. The names you see when calling a function (and passing in one or more arguments) are called argument labels.
+
+//func funcName(-argumentLabel-  -parameterName-: valueGoesHere){
+//    function logic goes here
+//}
+//funcName(argumentLabel: value)
+
+//You can just pass in the value for a function by having a ‘_’ for the functions argumentLabel
+
+//Functions and their arguments should be named so that they read like a clear instruction when they’re called. To make this easier, you can give parameters two names - an argument label to be used when calling the function and a parameter name to be used within the function’s body.
+
+//To write a function that can be called with an argument only, you use _ where you'd normally specify the argument label.
+
+
+
+func holler(_ phrase: String) -> String {
+    return "⚡️\(phrase)!!⚡️"
+}
+
+print(holler("Thank you, this is very nice."))
+print(holler("I'm not sure that was necessary."))
 
 
 
 
 
+//--------------------- Comparison Operators & If Else Statements --------------------
+
+//Booleans: true / false
+//
+//Comparison statements (end in a true or false)
+//
+//Comparison operators:
+//+, ==, -, *, /, <, >, <=, >=, !=, ||, %
+//
+//If
+//Else if
+//Else
 
 
+
+func bandCanCarryGear(bandMemberCount: Int, gearWeight: Int) -> Bool {
+    let maximumTripCount = 2
+    let weightPerPerson = 50
+    let carryingCapacity = bandMemberCount * weightPerPerson * maximumTripCount
+
+    return gearWeight < carryingCapacity
+}
+
+//: This approach hides the complexity of what’s happening in the function. Functions that return a `Bool` can be used directly in an if statement, like this:
+
+if bandCanCarryGear(bandMemberCount: 5, gearWeight: 600) {
+    "Rock on."
+} else {
+    "Everyone quits! Looks like you've got a solo show"
+}
+
+let bandMemberCount = 6
+let candyCount = 79
+
+func isCandyAmountAcceptable(bandMemberCount: Int, candyCount: Int) -> Bool {
+    return candyCount % bandMemberCount == 0
+}
+
+
+if isCandyAmountAcceptable(bandMemberCount: bandMemberCount, candyCount: candyCount) {
+    "Rock on."
+} else {
+    "Everyone quits! This is unacceptable!"
+}
+
+
+
+//--------------------- APIs --------------------
+//An API is a specific set of functionality that can be used by a software developer to accomplish a task.
+//When you develop an algorithm, you’re defining a series of steps to be performed. Any self-contained step-by-step series of operations is an algorithm.
 
 
 
